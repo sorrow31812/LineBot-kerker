cd757f2acc0c493dd7b93fcbd8d120d8f7585857
kewei <sorrow31812@gmail.com>
2018-12-24 18:13:12 +0800

    beauty_spider_test3

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    beauty_spider()
+    beauty_spider.main()
     # handle webhook body
     try:
         handler.handle(body, signature)
